5309dc43dee07f17960d6c3866fcec9779bf4877
Andreas Asatera <ryusuke005@gmail.com>
2020-12-22 18:49:50 +0700

    Fix config file
    - Change default value on create to false

--- config/user-activity.php
+++ config/user-activity.php
@@ -7,10 +7,10 @@ return [
     'delete_limit'     => 7,
 
     'log_events' => [
-        'on_create'    => true,
-        'on_edit'    => true,
-        'on_delete'  => true,
-        'on_login'   => true,
-        'on_lockout' => true
+        'on_create'     => false,
+        'on_edit'       => true,
+        'on_delete'     => true,
+        'on_login'      => true,
+        'on_lockout'    => true
     ]
 ];
