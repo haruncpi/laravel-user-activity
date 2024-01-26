3537441cb5cf6b9d533ce2c4ecc6f45c26be2ad0
SrFortes <41592754+SrFortes@users.noreply.github.com>
2024-01-26 13:36:46 -0400

    Update web.php
    change route get add name in route ->name('user-activity')

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ Route::group([
     'namespace' => '\Haruncpi\LaravelUserActivity\Controllers',
     'middleware' => config('user-activity.middleware')
     ], function () {
-    Route::get(config('user-activity.route_path'), 'ActivityController@getIndex');
+    Route::get(config('user-activity.route_path'), 'ActivityController@getIndex')->name('user-activity');
     Route::post(config('user-activity.route_path'), 'ActivityController@handlePostRequest');
 });
